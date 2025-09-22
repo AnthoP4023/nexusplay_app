5dee68d2ff252f50d8499881389d28e49b4d0812
AnthoP4023 <anthonyp200330@gmail.com>
2025-09-22 17:37:07 -0500

    start commit added

--- controladores/cont_agg_card.php
+++ controladores/cont_agg_card.php
@@ -130,40 +130,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['agregar_tarjeta'])) {
     }
 }
 
-function validarLuhn($numero) {
-    $numero = preg_replace('/\D/', '', $numero);
-    $longitud = strlen($numero);
-    $suma = 0;
-    
-    for ($i = $longitud - 1; $i >= 0; $i--) {
-        $digito = intval($numero[$i]);
-        
-        if (($longitud - $i) % 2 == 0) {
-            $digito *= 2;
-            if ($digito > 9) {
-                $digito -= 9;
-            }
-        }
-        
-        $suma += $digito;
-    }
-    
-    return ($suma % 10) == 0;
-}
-
-function detectarTipoTarjeta($numero) {
-    $numero = preg_replace('/\D/', '', $numero);
-    
-    if (preg_match('/^4/', $numero)) {
-        return 'Visa';
-    } elseif (preg_match('/^5[1-5]/', $numero)) {
-        return 'MasterCard';
-    } elseif (preg_match('/^3[47]/', $numero)) {
-        return 'American Express';
-    } elseif (preg_match('/^6(?:011|5)/', $numero)) {
-        return 'Discover';
-    } else {
-        return 'Desconocida';
-    }
-}
 ?>
