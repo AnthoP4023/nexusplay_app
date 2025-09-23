4658f6d361911cd7f113877b8a581beb9c436ab5
AnthoP4023 <anthonyp200330@gmail.com>
2025-09-23 11:26:02 -0500

    start commit added

--- functions/fun_search.php
+++ functions/fun_search.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config_db/database.php';
+require_once __DIR__ . '/../config_db/database.php';
 
 if (!function_exists('highlightSearchTerm')) {
     function highlightSearchTerm($text, $term) {
@@ -20,33 +20,42 @@ if (!function_exists('highlightSearchTerm')) {
 
 if (!function_exists('searchJuegos')) {
     function searchJuegos($conn, $search_query = '', $plataforma_id = '', $categoria_id = '', $precio = '') {
-        $sql = "SELECT j.id, j.titulo, j.descripcion, j.imagen, j.precio, j.desarrollador FROM juegos j WHERE 1=1";
+        $sql = "SELECT j.id, j.titulo, j.descripcion, j.imagen, j.precio, j.desarrollador 
+                FROM juegos j 
+                WHERE 1=1";
 
         if (!empty($search_query)) {
             $sql .= " AND j.titulo LIKE '%$search_query%'";
         }
+
         if (!empty($plataforma_id)) {
             $sql .= " AND j.plataforma_id = $plataforma_id";
         }
+
         if (!empty($categoria_id)) {
             $sql .= " AND j.categoria_id = $categoria_id";
         }
+
         if (!empty($precio)) {
             switch ($precio) {
                 case '1':
-                    $sql .= " AND j.precio < 20"; break;
+                    $sql .= " AND j.precio < 20";
+                    break;
                 case '2':
-                    $sql .= " AND j.precio BETWEEN 20 AND 50"; break;
+                    $sql .= " AND j.precio BETWEEN 20 AND 50";
+                    break;
                 case '3':
-                    $sql .= " AND j.precio > 50"; break;
+                    $sql .= " AND j.precio > 50";
+                    break;
             }
         }
 
         $result = $conn->query($sql);
         if ($result) {
             return $result->fetch_all(MYSQLI_ASSOC);
+        } else {
+            return [];
         }
-        return [];
     }
 }
 ?>
